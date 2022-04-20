8864c9ca81542776090ba40452cfae9680efaf0d
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-04-20 11:30:57 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "./components/Button.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Bored?</h1>
+      <h2>Press the button to get a suggestion on what to do</h2>
+      <Button buttonText="I'm bored"></Button>
     </div>
   );
 }
---
+++ src/components/Suggestion/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Suggestion = (props) => {
+  return <p>Hej</p>;
+};
+
+// Set which props this component takes
+// Button.propTypes = {
+//   buttonText: PropTypes.string,
+//   bg: PropTypes.string,
+// };
+
+// Set default props in case no props were passed to the component
+// Button.defaultProps = {
+//   buttonText: "I'm booored",
+//   bg: "",
+// };
+
+export default Suggestion;
