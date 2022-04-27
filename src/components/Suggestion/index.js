ad952d1e2d035a9d86fa5f243f8aa0db7ab97b6b
hanna-rosenberg <90154790+hanna-rosenberg@users.noreply.github.com>
2022-04-27 14:34:23 +0200

    useEffect works on all three buttons

--- src/components/Suggestion/index.js
+++ src/components/Suggestion/index.js
@@ -6,60 +6,63 @@ const Suggestion = () => {
   const [activity, setActivity] = useState(null);
   const [type, setType] = useState("");
   const [participants, setParticipants] = useState(0);
-
-  const [button, setButton] = useState(0);
+  const [buttonRandom, setButtonRandom] = useState(null);
+  const [buttonSingle, setButtonSingle] = useState(null);
+  const [buttonGroup, setButtonGroup] = useState(null);
 
   useEffect(() => {
     fetch("https://www.boredapi.com/api/activity")
       .then((response) => response.json())
-      .then((data) => setActivity(data.activity));
-  }, [button]);
+      .then((data) => {
+        setActivity(data.activity);
+        setType(data.type);
+        setParticipants(data.participants);
+      });
+  }, [buttonRandom]);
 
-  // const getSuggestion = async () => {
-  //   const response = await fetch("https://www.boredapi.com/api/activity");
-  //   const data = await response.json();
-  //   setActivity(data.activity);
-  //   setType(data.type);
-  //   setParticipants(data.participants);
-  // };
+  useEffect(() => {
+    fetch("https://www.boredapi.com/api/activity?participants=1")
+      .then((response) => response.json())
+      .then((data) => {
+        setActivity(data.activity);
+        setType(data.type);
+        setParticipants(data.participants);
+      });
+  }, [buttonSingle]);
 
-  // const getSingleSuggestion = async () => {
-  //   const response = await fetch(
-  //     "https://www.boredapi.com/api/activity?participants=1"
-  //   );
-  //   const data = await response.json();
-  //   setActivity(data.activity);
-  //   setType(data.type);
-  //   setParticipants(data.participants);
-  // };
+  useEffect(() => {
+    let randomInt = Math.floor(Math.random() * 8) + 2;
+    if (randomInt == 6 || randomInt == 7 || randomInt == 9) {
+      randomInt = Math.floor(Math.random() * 3) + 2;
+    }
+    fetch("https://www.boredapi.com/api/activity?participants=" + randomInt)
+      .then((response) => response.json())
+      .then((data) => {
+        setActivity(data.activity);
+        setType(data.type);
+        setParticipants(data.participants);
+      });
+  }, [buttonGroup]);
 
-  // const getGroupSuggestion = async () => {
-  //   let randomInt = Math.floor(Math.random() * 8) + 2;
-  //   if (randomInt == 6 || randomInt == 7 || randomInt == 9) {
-  //     randomInt = Math.floor(Math.random() * 3) + 2;
-  //   }
-  //   console.log(randomInt);
-  //   const response = await fetch(
-  //     "https://www.boredapi.com/api/activity?participants=" + randomInt
-  //   );
-  //   const data = await response.json();
-  //   setActivity(data.activity);
-  //   setType(data.type);
-  //   setParticipants(data.participants);
-  //   console.log(data);
-  // };
+  let randomInt = Math.floor(Math.random() * 8) + 2;
 
   const google = `https://letmegooglethat.com/?q=${activity}`;
 
   return (
     <div>
       <div className="buttonContainer">
-        <Button buttonText="Random" handleClick={() => setButton("changed")} />
-        <Button buttonText="Random" handleClick={() => setButton("changed2")} />
-        <Button buttonText="Random" handleClick={() => setButton("changed3")} />
-        {/* <Button buttonText="Random" handleClick={() => getSuggestion()} />
-        <Button buttonText="Single" handleClick={() => getSingleSuggestion()} />
-        <Button buttonText="Group" handleClick={() => getGroupSuggestion()} /> */}
+        <Button
+          buttonText="Random"
+          handleClick={() => setButtonRandom({ randomInt })}
+        />
+        <Button
+          buttonText="Single"
+          handleClick={() => setButtonSingle({ randomInt })}
+        />
+        <Button
+          buttonText="Group"
+          handleClick={() => setButtonGroup({ randomInt })}
+        />
       </div>
       <div className="cardContainer">
         {activity && (
@@ -67,13 +70,8 @@ const Suggestion = () => {
             <a href={google}>
               <h1>{activity}</h1>
             </a>
-<<<<<<< HEAD
             <p className="type">{type}</p>
             <p className="participants">{participants}</p>
-=======
-            <p class="type">Type: {type}</p>
-            <p class="participants">Participants: {participants}</p>
->>>>>>> cda8f2001e4fb6b718d1cccad0fe7df54a0e6ac4
           </div>
         )}
       </div>
